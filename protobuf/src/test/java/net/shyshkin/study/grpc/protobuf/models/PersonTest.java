d3205cc436823cd324d4ef124066e30be726ed9d
Art <d.art.shishkin@gmail.com>
2021-04-02 01:30:41 +0300

    11. Proto - Equals Method (#2)

--- protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/PersonTest.java
+++ protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/PersonTest.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.grpc.protobuf.models;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,5 +19,25 @@ class PersonTest {
                 () -> assertEquals("Art", person.getName()),
                 () -> assertEquals(38, person.getAge())
         );
+    }
+
+    @Test
+    @DisplayName("Persons with same field values must match")
+    void equalsAndHashcodeTest() {
+        //given
+        Person person1 = Person.newBuilder()
+                .setName("Art")
+                .setAge(38)
+                .build();
+
+        //when
+        Person person2 = Person.newBuilder()
+                .setName("Art")
+                .setAge(38)
+                .build();
+
+        //then
+        assertEquals(person1, person2);
+        assertEquals(person1.hashCode(), person2.hashCode());
     }
 }
