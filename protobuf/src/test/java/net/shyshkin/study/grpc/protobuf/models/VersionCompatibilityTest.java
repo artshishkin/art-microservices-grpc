43fd7de6378a725f1a9529ddc127ec9fff87bc00
Art <d.art.shishkin@gmail.com>
2021-04-02 23:21:16 +0300

    33. Proto - API/Message Changes - Part - 2_2 (#2)

--- protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/VersionCompatibilityTest.java
+++ protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/VersionCompatibilityTest.java
@@ -34,6 +34,7 @@ class VersionCompatibilityTest {
     }
 
     @Test
+    @Disabled("Now version is v1")
     void v2_test_read_v1() throws IOException {
         //given
         Path path = Paths.get("tv-v1");
@@ -45,19 +46,20 @@ class VersionCompatibilityTest {
         //then
         System.out.println(savedTelevision);
         assertAll(
-                () -> assertEquals("sony", savedTelevision.getBrand()),
-                () -> assertEquals(2005, savedTelevision.getModel()),
-                () -> assertEquals(Type.UNDEFINED, savedTelevision.getType())
+//                () -> assertEquals(2005, savedTelevision.getModel()),
+//                () -> assertEquals(Type.UNDEFINED, savedTelevision.getType()),
+        () -> assertEquals("sony", savedTelevision.getBrand())
         );
     }
 
     @Test
+    @Disabled("Now version is v1")
     void v2_test_create_v2() throws IOException {
         //given
         Television television = Television.newBuilder()
                 .setBrand("sony")
-                .setModel(12345)
-                .setType(Type.OLED)
+//                .setModel(12345)
+//                .setType(Type.OLED)
                 .build();
 
         //when
@@ -70,5 +72,22 @@ class VersionCompatibilityTest {
         //then
         System.out.println(savedTelevision);
         assertEquals(television, savedTelevision);
+    }
+
+    @Test
+    void v1_test_read_v2() throws IOException {
+        //given
+        Path path = Paths.get("tv-v2");
+
+        //when
+        byte[] bytes = Files.readAllBytes(path);
+        Television savedTelevision = Television.parseFrom(bytes);
+
+        //then
+        System.out.println(savedTelevision);
+        assertAll(
+                () -> assertEquals("sony", savedTelevision.getBrand()),
+                () -> assertEquals(12345, savedTelevision.getYear())
+        );
     }
 }
