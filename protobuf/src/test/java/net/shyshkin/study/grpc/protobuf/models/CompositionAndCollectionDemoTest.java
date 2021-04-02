8c3f462181c694bb2a68e2be58b87cd75bcfc07d
Art <d.art.shishkin@gmail.com>
2021-04-02 16:38:38 +0300

    24. Proto - Map Demo (#2)

--- protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/CompositionAndCollectionDemoTest.java
+++ protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/CompositionAndCollectionDemoTest.java
@@ -24,9 +24,9 @@ class CompositionAndCollectionDemoTest {
                 .setYear(2008);
 
         Car newCar = Car.newBuilder()
-                .setBrand("Daewoo")
-                .setModel("Lanos")
-                .setYear(2008)
+                .setBrand("Ford")
+                .setModel("Escape")
+                .setYear(2015)
                 .build();
 
         Person person = Person.newBuilder()
@@ -47,5 +47,32 @@ class CompositionAndCollectionDemoTest {
                 () -> assertEquals("Lanos", person.getCar(0).getModel()),
                 () -> assertEquals(2008, person.getCar(0).getYear())
         );
+    }
+
+    @Test
+    void mapDemo() {
+        //given
+        Car escape = Car.newBuilder()
+                .setBrand("Ford")
+                .setModel("Escape")
+                .setYear(2015)
+                .build();
+
+        Car journey = Car.newBuilder()
+                .setBrand("Dodge")
+                .setModel("Journey")
+                .setYear(2014)
+                .build();
+
+        //when
+        Dealer dealer = Dealer.newBuilder()
+                .putCarPosition(54321, escape)
+                .putCarPosition(54322, journey)
+                .build();
+
+        //then
+        assertEquals(escape, dealer.getCarPositionOrThrow(54321));
+        assertEquals(2, dealer.getCarPositionCount());
+        System.out.println(dealer);
     }
 }
