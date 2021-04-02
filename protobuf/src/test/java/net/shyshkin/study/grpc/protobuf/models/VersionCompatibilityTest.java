2410c59d105ef83b5a19bb4c6042488011465fb2
Art <d.art.shishkin@gmail.com>
2021-04-02 23:13:50 +0300

    33. Proto - API/Message Changes - Part - 2 (#2)

--- protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/VersionCompatibilityTest.java
+++ protobuf/src/test/java/net/shyshkin/study/grpc/protobuf/models/VersionCompatibilityTest.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.grpc.protobuf.models;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,16 +8,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VersionCompatibilityTest {
 
     @Test
+    @Disabled("Was for testing in `32. Proto - API/Message Changes - Part - 1` and creation of `tv-v1` file")
     void v1_test() throws IOException {
         //given
         Television television = Television.newBuilder()
                 .setBrand("sony")
-                .setYear(2005)
+//                .setYear(2005)
                 .build();
 
         //when
@@ -27,6 +30,45 @@ class VersionCompatibilityTest {
         Television savedTelevision = Television.parseFrom(bytes);
 
         //then
+        assertEquals(television, savedTelevision);
+    }
+
+    @Test
+    void v2_test_read_v1() throws IOException {
+        //given
+        Path path = Paths.get("tv-v1");
+
+        //when
+        byte[] bytes = Files.readAllBytes(path);
+        Television savedTelevision = Television.parseFrom(bytes);
+
+        //then
+        System.out.println(savedTelevision);
+        assertAll(
+                () -> assertEquals("sony", savedTelevision.getBrand()),
+                () -> assertEquals(2005, savedTelevision.getModel()),
+                () -> assertEquals(Type.UNDEFINED, savedTelevision.getType())
+        );
+    }
+
+    @Test
+    void v2_test_create_v2() throws IOException {
+        //given
+        Television television = Television.newBuilder()
+                .setBrand("sony")
+                .setModel(12345)
+                .setType(Type.OLED)
+                .build();
+
+        //when
+        Path path = Paths.get("tv-v2");
+        Files.write(path, television.toByteArray());
+
+        byte[] bytes = Files.readAllBytes(path);
+        Television savedTelevision = Television.parseFrom(bytes);
+
+        //then
+        System.out.println(savedTelevision);
         assertEquals(television, savedTelevision);
     }
 }
