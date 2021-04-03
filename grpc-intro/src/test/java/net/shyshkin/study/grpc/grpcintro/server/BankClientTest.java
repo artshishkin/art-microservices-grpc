08b6c7cd026a1d413656608ecb18ec9ec74e2149
Art <d.art.shishkin@gmail.com>
2021-04-03 10:45:34 +0300

    44. Unary - Client's Blocking Stub (#3)

---
+++ grpc-intro/src/test/java/net/shyshkin/study/grpc/grpcintro/server/BankClientTest.java
@@ -0,0 +1,65 @@
+package net.shyshkin.study.grpc.grpcintro.server;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import net.shyshkin.study.grpc.grpcintro.models.Balance;
+import net.shyshkin.study.grpc.grpcintro.models.BalanceCheckRequest;
+import net.shyshkin.study.grpc.grpcintro.models.BankServiceGrpc;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class BankClientTest {
+
+    private static BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    private static Server server;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+
+        server = ServerBuilder
+                .forPort(6565)
+                .addService(new BankService())
+                .build();
+
+        System.out.println("Starting gRPC server");
+
+        server.start();
+
+        ManagedChannel managedChannel = ManagedChannelBuilder
+                .forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+        blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Shutdown gRPC server");
+        server.shutdown();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 7})
+    void balanceTest(int accountNumber) {
+        //given
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(accountNumber)
+                .build();
+
+        //when
+        Balance balance = blockingStub.getBalance(balanceCheckRequest);
+
+        //then
+        System.out.printf("Received balance: %d for user %d", balance.getAmount(), accountNumber);
+        assertEquals(accountNumber * 111, balance.getAmount());
+    }
+}
