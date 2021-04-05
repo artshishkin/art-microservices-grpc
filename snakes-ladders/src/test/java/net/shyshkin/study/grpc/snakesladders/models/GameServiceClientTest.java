cfaf4910ebc3a39869816a5415b179517dfdb66b
Art <d.art.shishkin@gmail.com>
2021-04-05 11:40:22 +0300

    80. Game - Players State Streaming Observer Implementation (my impl) (#7)

---
+++ snakes-ladders/src/test/java/net/shyshkin/study/grpc/snakesladders/models/GameServiceClientTest.java
@@ -0,0 +1,121 @@
+package net.shyshkin.study.grpc.snakesladders.models;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.grpc.snakesladders.engine.GameEngine;
+import net.shyshkin.study.grpc.snakesladders.engine.GameEngineImpl;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+class GameServiceClientTest {
+
+    private static GameServiceGrpc.GameServiceStub nonBlockingStub;
+    private static Server server;
+    private CountDownLatch countDownLatch;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+
+        GameEngine gameEngine = new GameEngineImpl();
+        GameService gameService = new GameService(gameEngine);
+
+        server = ServerBuilder
+                .forPort(6363)
+                .addService(gameService)
+                .build();
+
+        log.debug("Starting gRPC server");
+
+        server.start();
+
+        ManagedChannel managedChannel = ManagedChannelBuilder
+                .forAddress("localhost", server.getPort())
+                .usePlaintext()
+                .build();
+        nonBlockingStub = GameServiceGrpc.newStub(managedChannel);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        log.debug("Shutdown gRPC server");
+        server.shutdown();
+    }
+
+    @BeforeEach
+    void setUp() {
+        countDownLatch = new CountDownLatch(1);
+    }
+
+    @AfterEach
+    void tearDown() throws InterruptedException {
+        countDownLatch.await();
+    }
+
+    @Test
+    void gameTest() {
+        //given
+        TestGameStateStreamObserver gameStateResponseObserver = new TestGameStateStreamObserver();
+        StreamObserver<Die> clientDieStreamObserver = nonBlockingStub.roll(gameStateResponseObserver);
+        gameStateResponseObserver.setDieStreamObserver(clientDieStreamObserver);
+
+        //when
+        int dieValue = ThreadLocalRandom.current().nextInt(1, 7);
+        Die firstDie = Die.newBuilder().setValue(dieValue).build();
+        clientDieStreamObserver.onNext(firstDie);
+
+        //then
+
+    }
+
+    private class TestGameStateStreamObserver implements StreamObserver<GameState> {
+
+        public static final int FINAL_POSITION = 100;
+
+        @Setter
+        private StreamObserver<Die> dieStreamObserver;
+
+        @Override
+        public void onNext(GameState gameState) {
+
+            Player client = gameState.getPlayer(0);
+            Player server = gameState.getPlayer(1);
+
+            if (client.getPosition() == FINAL_POSITION || server.getPosition() == FINAL_POSITION) {
+                log.debug("Game Over");
+                String winner = client.getPosition() == FINAL_POSITION ? client.getName() : server.getName();
+                log.debug("{} wins", winner);
+                dieStreamObserver.onCompleted();
+                return;
+            }
+            dieStreamObserver.onNext(newDie());
+        }
+
+        private Die newDie() {
+            int dieValue = ThreadLocalRandom.current().nextInt(1, 7);
+            return Die.newBuilder().setValue(dieValue).build();
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            log.error("Exception occurred", t);
+            countDownLatch.countDown();
+            dieStreamObserver.onCompleted();
+        }
+
+        @Override
+        public void onCompleted() {
+            log.debug("onCompleted");
+            countDownLatch.countDown();
+//            dieStreamObserver.onCompleted();
+        }
+    }
+}
