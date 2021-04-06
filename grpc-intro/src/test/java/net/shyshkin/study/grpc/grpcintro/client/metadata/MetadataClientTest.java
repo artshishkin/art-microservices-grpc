c899702c57c3485802591d88e9a848df46324498
Art <d.art.shishkin@gmail.com>
2021-04-07 00:04:03 +0300

    111. Interceptor - Client Service Token Validation - Demo (#10)

--- grpc-intro/src/test/java/net/shyshkin/study/grpc/grpcintro/client/metadata/MetadataClientTest.java
+++ grpc-intro/src/test/java/net/shyshkin/study/grpc/grpcintro/client/metadata/MetadataClientTest.java
@@ -1,16 +1,16 @@
 package net.shyshkin.study.grpc.grpcintro.client.metadata;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.*;
 import io.grpc.stub.MetadataUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.grpc.grpcintro.models.*;
 import net.shyshkin.study.grpc.grpcintro.server.deadline.DeadlineService;
+import net.shyshkin.study.grpc.grpcintro.server.metadata.AuthInterceptor;
 import net.shyshkin.study.grpc.grpcintro.server.rpctypes.AccountDatabase;
-import org.junit.jupiter.api.AfterAll;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -18,45 +18,56 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class MetadataClientTest {
 
+    public static final String VALID_TOKEN = "SuperSecretToken";
+    public static final String INVALID_TOKEN = "InvalidToken";
+
     private static BankServiceGrpc.BankServiceBlockingStub blockingStub;
     private static Server server;
+    private static AccountDatabase accountDatabase;
 
     @BeforeAll
     static void beforeAll() throws IOException {
 
-        AccountDatabase accountDatabase = new AccountDatabase();
-
-        server = ServerBuilder
-                .forPort(6363)
-                .addService(new DeadlineService(accountDatabase))
-                .build();
-
-        log.debug("Starting gRPC server");
-
-        server.start();
+        accountDatabase = new AccountDatabase();
 
         ManagedChannel managedChannel = ManagedChannelBuilder
                 .forAddress("localhost", 6363)
                 .intercept(MetadataUtils.newAttachHeadersInterceptor(ClientConstants.getClientToken()))
                 .usePlaintext()
                 .build();
+
         blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
     }
 
-    @AfterAll
-    static void afterAll() {
+    private static void startServerWIthClientToken(String token) throws IOException {
+        server = ServerBuilder
+                .forPort(6363)
+                .intercept(new AuthInterceptor(token))
+                .addService(new DeadlineService(accountDatabase))
+                .build();
+
+        log.debug("Starting gRPC server");
+        server.start();
+    }
+
+    @AfterEach
+    void tearDown() {
         log.debug("Shutdown gRPC server");
         server.shutdown();
     }
 
     @Test
-    void balanceTest_OK() {
+    @DisplayName("When tokens on client and server sides match request should be processed")
+    void balanceTest_OK() throws IOException {
         //given
+        startServerWIthClientToken(VALID_TOKEN);
+
         int accountNumber = 33;
         BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
                 .setAccountNumber(accountNumber)
@@ -72,8 +83,34 @@ class MetadataClientTest {
     }
 
     @Test
-    void withdrawTest() {
+    @DisplayName("When tokens on client and server sides do not match request should be rejected")
+    void balanceTest_FAIL() throws IOException {
         //given
+        startServerWIthClientToken(INVALID_TOKEN);
+
+        int accountNumber = 33;
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(accountNumber)
+                .build();
+
+        //when
+        ThrowableAssert.ThrowingCallable exec = () -> {
+            Balance balance = blockingStub
+                    .getBalance(balanceCheckRequest);
+            log.debug("Received balance: {} for user {}", balance.getAmount(), accountNumber);
+            assertEquals(accountNumber * 111, balance.getAmount());
+        };
+
+        //then
+        assertThatThrownBy(exec).isInstanceOf(StatusRuntimeException.class)
+                .hasMessage("UNAUTHENTICATED: invalid/expired token");
+    }
+
+    @Test
+    void withdrawTest_validToken() throws IOException {
+        //given
+        startServerWIthClientToken(VALID_TOKEN);
+
         WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder()
                 .setAccountNumber(10)
                 .setAmount(40)
@@ -88,5 +125,31 @@ class MetadataClientTest {
         moneyIterator.forEachRemaining(moneyList::add);
         assertEquals(4, moneyList.size());
         moneyList.forEach(money -> assertEquals(10, money.getValue()));
+    }
+
+    @Test
+    void withdrawTest_fail() throws IOException {
+        //given
+        startServerWIthClientToken(INVALID_TOKEN);
+
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder()
+                .setAccountNumber(10)
+                .setAmount(40)
+                .build();
+
+        //when
+        ThrowableAssert.ThrowingCallable exec = () -> {
+            Iterator<Money> moneyIterator = blockingStub
+                    .withdraw(withdrawRequest);
+            List<Money> moneyList = new ArrayList<>();
+            moneyIterator.forEachRemaining(moneyList::add);
+            assertEquals(4, moneyList.size());
+            moneyList.forEach(money -> assertEquals(10, money.getValue()));
+        };
+
+        //then
+        assertThatThrownBy(exec)
+                .isInstanceOf(StatusRuntimeException.class)
+                .hasMessage("UNAUTHENTICATED: invalid/expired token");
     }
 }
